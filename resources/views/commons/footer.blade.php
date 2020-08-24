db2c98fb243ba157d4d7ac66b30c8e3223b14395
Takano <anpanmannoatarashiikao@gmail.com>
2020-08-24 19:16:40 +0900

    footer修正

--- resources/views/commons/footer.blade.php
+++ resources/views/commons/footer.blade.php
@@ -1,13 +1,8 @@
 <footer class= "bgcolor-blue mt-5">
-        <div class="m-3">
-
-                <div>
+        
+                <div class="px-3 py-2">
                         <h4 class="text-white">YunoCode<h4>
-                </div>
-
-                <div>
                         <h6 class="text-white text-right">© QuestAcademia, All rights reserved.<h6>
-                </div>       
+                </div>     
 
-        </div>
 </footer>
