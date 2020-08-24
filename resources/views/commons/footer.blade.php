3de543a153b3cf386671cb6f118aaf5cd5f29d0c
Takano <anpanmannoatarashiikao@gmail.com>
2020-08-24 18:58:51 +0900

    FrontTopPage作成

---
+++ resources/views/commons/footer.blade.php
@@ -0,0 +1,13 @@
+<footer class= "bgcolor-blue mt-5">
+        <div class="m-3">
+
+                <div>
+                        <h4 class="text-white">YunoCode<h4>
+                </div>
+
+                <div>
+                        <h6 class="text-white text-right">© QuestAcademia, All rights reserved.<h6>
+                </div>       
+
+        </div>
+</footer>
