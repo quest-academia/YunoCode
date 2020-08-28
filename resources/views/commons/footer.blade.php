6fc8ddd1ac9e8a34e34f320d1f4170787db564c1
Takano <anpanmannoatarashiikao@gmail.com>
2020-08-28 17:20:24 +0900

    fix:picture

---
+++ resources/views/commons/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class= "bgcolor-blue mt-5">
+        
+                <div class="px-3 py-2">
+                        <h4 class="text-white">YunoCode<h4>
+                        <h6 class="text-white text-right">© QuestAcademia, All rights reserved.<h6>
+                </div>     
+
+</footer>
---
+++ resources/views/commons/header.blade.php
@@ -0,0 +1,29 @@
+<header class="mb-5">
+
+    
+
+    <nav class="navbar navbar-expand-lg navbar-dark bgcolor-blue">
+
+        
+        <a class="navbar-brand white-text" href="/">YunoCode</a>
+
+        <button type="button" class="navbar-toggler white-text" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon" ></span>
+        </button>
+
+        
+        
+
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                <li class="nav-item"><a href="" class="nav-link  white-text">商品一覧</a></li>
+                <li class="nav-item"><a href="" class="nav-link  white-text">商品登録</a></li>
+                <li class="nav-item"><a href="" class="nav-link  white-text">〇〇さん</a></li>
+                <li class="nav-item"><a href="" class="nav-link  white-text">ログアウト</a></li>
+            </ul>
+        </div>
+
+    </nav>
+
+</header>
