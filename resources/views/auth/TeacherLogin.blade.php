6fb6d970e27e8b65de52b4cd0c2736fb1a263747
Akrjourney <[akarikuma1993@gmail.com]>
2020-11-03 08:54:14 +0000

    web.php変更

--- resources/views/auth/TeacherLogin.blade.php
+++ resources/views/auth/TeacherLogin.blade.php
@@ -1,13 +1,11 @@
-@extends('layouts.app')
-
 @section('content')
 
-    <div class="text-center navy-text">
-        <h1 class="login_title text-center text-navy mt-5">講師ログイン</h1>
+    <div class="text-center blue-text">
+        <h1 class="login_title text-center text-blue mt-5">講師ログイン</h1>
     </div>
 
     <div class="row mt-3 mb-1">
-        <div class="col-sm-12">
+        <div class="col-sm-6 offset-sm-3">
 
               <div class="form-group">
                   {!! Form::label('email', 'メールアドレス') !!}
@@ -22,14 +20,16 @@
           </div>
         </div>
 
-        <div class="text-center col-sm-12">
+        <div class="text-center mt-3">
 
+        <div class="text-center">
                 {!! Form::submit('ログイン', ['class' => 'btn btn btn-primary mt-2']) !!}
                 {!! Form::close() !!}
+        </div>
 
               <p class="mt-3">{!! link_to_route('/', '初めての方はこちらから') !!}</p>
               {!! Form::close() !!}
-        </div>
+        
     </div>
     
 @endsection
